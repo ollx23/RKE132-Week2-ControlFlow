0dcb7cd7ba22146f60daa2a7272c5fd7d54a1e1d
Balja. <gohot.poolggmail.com.>
2024-04-26 00:27:17 +0300

    aff week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Enter your age: ");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isageNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isageNumber}. User is {userAgeNum} years old");
+
+if (isageNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
+
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
